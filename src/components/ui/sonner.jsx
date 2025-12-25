0564b5a984c43183ffa4196678d42fc8b9b4a7ba
“maymasasa” <soozmas@gmail.com>
2025-12-25 21:52:33 +0200

    refactor: migrate toast notification system to use `sonner` library with updated styling.

--- src/components/ui/sonner.jsx
+++ src/components/ui/sonner.jsx
@@ -10,11 +10,13 @@ const Toaster = ({
   return (
     (<Sonner
       theme={theme}
-      className="toaster group"
+      richColors
+      dir="rtl"
+      className="toaster group font-['Rubik']"
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg",
+            "group toast group-[.toaster]:bg-background group-[.toaster]:text-foreground group-[.toaster]:border-border group-[.toaster]:shadow-lg font-['Rubik'] data-[type=success]:!bg-green-500 data-[type=success]:!text-white data-[type=success]:!border-green-600 data-[type=error]:!bg-red-600 data-[type=error]:!text-white data-[type=error]:!border-red-700",
           description: "group-[.toast]:text-muted-foreground",
           actionButton:
             "group-[.toast]:bg-primary group-[.toast]:text-primary-foreground",
